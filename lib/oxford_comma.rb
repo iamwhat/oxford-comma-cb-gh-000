cf053dc382c3eb88267a4a86376d46039da1b0b1
William Li <githuberr@outlook.com>
2018-05-19 00:01:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
   array[-1] = "and " + array.last
   array.join(", ")
+  if array.length <= 1
+    array
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+    array[-1] = 'and ' + array.last
+    array.join(", ")
 end
